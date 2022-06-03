76949f9294f8252a10fa37d345e0be72a9bbc838
ezequiel <ezequielgl27@gmail.com>
2022-06-02 22:23:54 -0300

    agregado de NavBar

---
+++ src/Components/App.js
@@ -0,0 +1,14 @@
+import NavBar from "./NavBar";
+import Footer from "./Footer";
+import Main from "./Main";
+const App = () =>{
+    return (
+        <>
+            <NavBar/>
+            <Main/>
+            <Footer/>  
+        </>
+    )  
+} ;
+
+export default App; 
---
+++ src/Components/NavBar.js
@@ -0,0 +1,12 @@
+const NavBar = () => {
+    return (<NavBar>
+        <h1>tiutlo</h1>
+        <nav>
+            <a href="#">link </a>
+            <a href="#">link </a>
+            <a href="#">link </a>
+        </nav>
+    </NavBar>)
+}
+
+export default NavBar 
