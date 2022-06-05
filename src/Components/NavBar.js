bcff3fbd344f00acc7ab7e465a453bdbd8d1be6b
ezequiel <ezequielgl27@gmail.com>
2022-06-05 19:45:45 -0300

    correccion de navbar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,12 +1,12 @@
 const NavBar = () => {
-    return (<NavBar>
+    return (<>
         <h1>tiutlo</h1>
         <nav>
             <a href="#">link </a>
             <a href="#">link </a>
             <a href="#">link </a>
         </nav>
-    </NavBar>)
+    </>)
 }
 
 export default NavBar 
