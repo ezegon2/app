031a756e0a704a66840dbab7c7b39589c3221ee1
ezequiel <ezequielgl27@gmail.com>
2022-06-20 19:02:12 -0300

    arreglo de contador y mejora de css

--- src/Components/Item.js
+++ src/Components/Item.js
@@ -5,23 +5,14 @@ import style from './Css/Item.module.css'
 
 function Item({id, name, price, stock,image,description}) {
   return (
-    <div className={style.container} >
-        <h1>{name}</h1>
-        <img src={image} alt={id} />
-        <ItemCount stock={stock}/>
-  
     <div className={style.container}>
       <h2>{name}</h2>
-
       <div className={style.card}>
         <img src={image} alt={id} />
-
         <p className={style.price}>{price}</p>
-
         <Link className={style.buttonCard} to={`/detail/${id}`}>Ver Detalle</Link>
       </div>
     </div>
-  </div>
   )
 }
 
